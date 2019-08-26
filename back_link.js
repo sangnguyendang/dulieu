d4be6317d5ac1f0617c149a91db8724df8dff25c
sangnguyendang <46393734+sangnguyendang@users.noreply.github.com>
2019-08-26 09:29:54 +0700

    Update back_link.js

--- back_link.js
+++ back_link.js
@@ -1,13 +1,15 @@
-(function(){
-    var d = document;
-    var l = "https://butaba.com";
-    var n = d.getElementsByTagName('script')[0];
-    var s = d.createElement('script');
-    s.type = 'text/javascript';
-    s.src = l + '/api/zuda.js';
-    s.async = 1;
-    n.parentNode.insertBefore(s, n);
-    window.onload = function(e) {
-      ZUDA.init("https://zuda.vn/products/merawhite?company=merawhite"); // Điền địa chỉ cần back về
-    }
-  })();
+ <script type="text/javascript">
+  (function () {
+  var d = document;
+  var l = "https://butaba.com";
+  var n = d.getElementsByTagName('script')[0];
+  var s = d.createElement('script');
+  s.type = 'text/javascript';
+  s.src = l + '/api/zuda.js';
+  s.async = 1;
+  n.parentNode.insertBefore(s, n);
+  window.onload = function (e) {
+    ZUDA.init("https://zuda.vn/products/merawhite?company=merawhite"); // Điền địa chỉ cần back về
+  }
+})();
+</script>
